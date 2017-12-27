e25827f546c466ad8cf23eaf461c99626863074b
Ville Oskari Rantamäki <viosrant@student.jyu.fi>
2017-12-27 21:49:05 +0200

    Muokattu kokonaan uusiksi

--- ConsoleMain1/ConsoleMain1.cs
+++ ConsoleMain1/ConsoleMain1.cs
@@ -11,45 +11,26 @@ namespace Yle
 {
     public class Excel_Testi
     {
-        private static double[,] arvot = new double[300, 4];
+        private static Object[,] arvot = new Object[300, 90];
+        
+        //Tässä alustetaan Excel-tiedosto käyttöön
+        private static Excel.Application xlApp = new Excel.Application();
+        private static Excel.Workbooks xlWorkbookS = xlApp.Workbooks;
+        private static Excel.Workbook xlWorkbook = xlWorkbookS.Open(@"D:\repos\ConsoleMain1\ConsoleMain1\Resources\Kuntatutka.xlsx");
+        private static Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
+        private static Excel.Range xlRange = xlWorksheet.UsedRange;
+        private static int rowCount = xlRange.Rows.Count;
+        private static int colCount = xlRange.Columns.Count;
 
         public static void Main()
         {
-            LueExcelista();
-            LaskeKeskiarvot(arvot);
+            LaskeKeskiarvot();
+            Console.Write("\r\nSyötä arvioitava maakunta (muista isot alkukirjaimet):");
+            string maakunta = Console.ReadLine();
+            LaskeMaakunnat(maakunta);
+
+            Console.WriteLine("\r\nPaina mitä vaan poistuaksesi.");
             Console.ReadKey();
-        }
-
-        public static double[,] LueExcelista()
-        {
-
-            //Tässä alustetaan Excel-tiedosto käyttöön
-            Excel.Application xlApp = new Excel.Application();
-            Excel.Workbooks xlWorkbookS = xlApp.Workbooks;
-            Excel.Workbook xlWorkbook = xlWorkbookS.Open(@"D:\repos\ConsoleMain1\ConsoleMain1\Resources\Kuntatutka.xlsx");
-            Excel._Worksheet xlWorksheet = xlWorkbook.Sheets[1];
-            Excel.Range xlRange = xlWorksheet.UsedRange;
-
-            
-            int arvotrow = 0;
-            int arvotcol = 0;
-
-            int rowCount = xlRange.Rows.Count;
-            int colCount = xlRange.Columns.Count;
-
-            
-
-            //Tässä laitetaan Excel tarkastamaan solut rivistä 2 alkaen, sarakkeet 47-50 eli AU-AX ja tallennetaan ne taulukkoon
-            for (int i = 2; i <= rowCount; i++)
-            {
-                for (int j = 47; j <= 50; j++)
-                {
-                    arvotrow = i - 2;
-                    arvotcol = j - 47;
-                    double arvo = Convert.ToDouble(xlRange.Cells[i, j].Value2);
-                    arvot[arvotrow, arvotcol] = arvo;
-                }
-            }
 
             //cleanup
             GC.Collect();
@@ -59,35 +40,67 @@ namespace Yle
             Marshal.ReleaseComObject(xlRange);
             Marshal.ReleaseComObject(xlWorksheet);
 
-            //Sulkeminen ja vapauttaminen
+            //Työkirjan sulkeminen ja vapauttaminen
             xlWorkbook.Close();
             Marshal.ReleaseComObject(xlWorkbook);
             Marshal.ReleaseComObject(xlWorkbookS);
             xlApp.Quit();
             Marshal.ReleaseComObject(xlApp);
 
-            return arvot;
-
         }
 
-        public static void LaskeKeskiarvot(double[,] arvot)
+        //Tälle funktiolle syötetään lähtöarvona laskettava taulukko, josta se laskee sarakkeiden keskiarvot
+        public static void LaskeKeskiarvot()
         {
 
             double temp = 0;
+            int maara = 0;
 
-            for (int j = 0; j < arvot.GetLength(1); j++)
+            for (int j = 47; j <=50; j++)
             {
-                for (int i = 0; i < arvot.GetLength(0); i++)
+                maara = 0;
+                temp = 0;
+
+                for (int i = 2; i <= rowCount; i++)
                 {
-                    temp = temp+arvot[i, j];
+                    temp = temp+Convert.ToDouble(xlRange.Cells[i, j].Value2);
+                    maara++;
                 }
 
                 double keskiarvo = temp / 295;
-                Console.WriteLine((j+1).ToString() + ". sarakkeen arvojen summa: " + temp.ToString());
-                Console.WriteLine((j + 1).ToString() + ". sarakkeen arvojen keskiarvo: " + keskiarvo.ToString());
-
-                temp = 0;
+                keskiarvo = Math.Round(keskiarvo, 2);
+                
+                Console.WriteLine("\r\n" + (j).ToString() + ". sarakkeen arvojen keskiarvo koko maassa: " + keskiarvo.ToString());
             }
+            Console.WriteLine("\r\nKuntia koko Suomessa: " + maara.ToString());
+        }
+        
+        //Tämä funktio ottaa syötteenä halutun maakunnan nimen, ja antaa sitä koskevien tunnuslukujen keskiarvot
+        public static void LaskeMaakunnat(string s)
+        {
+            double temp = 0;
+            int maara = 0;
+
+            for (int j = 47; j <= 50; j++)
+            {
+                maara = 0;
+                temp = 0;
+
+                for (int i = 2; i <= rowCount; i++)
+                {
+                    if (xlRange.Cells[i, 4].Value2 == s && xlRange.Cells[i, j].Value2 != null)
+                    {
+                        temp = temp + Convert.ToDouble(xlRange.Cells[i, j].Value2);
+                        maara++;
+                    }
+                }
+
+                double keskiarvo = temp / maara;
+                keskiarvo = Math.Round(keskiarvo, 2);
+
+                Console.WriteLine("\r\n" + (j).ToString() + ". sarakkeen arvojen keskiarvo" + "(" + s + "): " + keskiarvo.ToString());
+            }
+            Console.WriteLine("\r \nKuntia alueella " + s + ": " + maara.ToString());
         }
     }
 }
